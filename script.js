15b81aad48ef352de81d3144d407da6514b20de4
juan <juan.pedro.casanas99@gmail.com>
2021-08-19 18:58:47 -0300

    Same changes as live version, added tab icon

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const html = document.querySelector('html');
+const body = document.querySelector('body');
 const drawingBoard = document.getElementById('drawingBoard');
 const clear = document.getElementById('clear')
 const eraser = document.getElementById('eraser');
@@ -6,6 +7,7 @@ const bgColorSelector = document.getElementById('bgColor');
 const brushColor = document.getElementById('brushColor');
 const bgColorButton = document.getElementById('bgColorButton');
 const brushColorButton = document.getElementById('brushColorButton');
+const rainbow = document.getElementById('rainbow');
 
 let sketchSize = document.getElementById('sketchSize');
 let sketchGrid;
@@ -14,12 +16,13 @@ let bgColor = bgColorSelector.value;
 
 sketchSize.value = 16; createGrid();
 sketchSize.addEventListener('change', () => { createGrid() })
+body.style.backgroundColor = hexToRGB(bgColor);
 clear.addEventListener('click', clearBoard);
-eraser.addEventListener('click', selectEraser);
-bgColorButton.addEventListener('click', selectBgColor);
-brushColorButton.addEventListener('click', selectColor);
+eraser.addEventListener('click', () => { selectMode('eraserButton') });
+rainbow.addEventListener('click', () => { selectMode('rainbowButton') });
+brushColorButton.addEventListener('click', () => { selectMode('colorButton') });
 html.addEventListener('mouseup', toggleColor);
-brushColor.addEventListener('change', selectColor);
+brushColor.addEventListener('change', () => { selectMode('colorButton') });
 bgColorSelector.addEventListener('change', selectBgColor);
 
 
@@ -36,16 +39,18 @@ function createGrid() {
   drawingBoard.style.gridTemplateRows = `repeat(${sketchSize.value}, 1fr)`;
 }
 
-function selectColor() {
-  color = brushColor.value;
-}
 
 function selectBgColor() {
   bgColor = bgColorSelector.value
   let array = Array.from(drawingBoard.children)
   array.forEach((e) => {
     if (e.className == 'grid painted') {
-
+    } else if (eraser.classList == 'clickedEraser') {
+      Object.assign(e.style, {
+        backgroundColor: bgColor,
+        opacity: 1
+      });
+      color = bgColor;
     } else {
       Object.assign(e.style, {
         backgroundColor: bgColor,
@@ -53,15 +58,17 @@ function selectBgColor() {
       });
     }
   })
+  body.style.backgroundColor = hexToRGB(bgColor);
 }
 
 function paint(e) {
   let array = Array.from(drawingBoard.children)
   array.forEach((e) => { e.addEventListener('mouseenter', paint); })
   Object.assign(e.target.style, {
-    backgroundColor: color,
+    backgroundColor: colorSelector(),
     opacity: 1
   });
+
   if (color != bgColor) {
     e.target.classList.add('painted');
     e.stopPropagation();
@@ -71,7 +78,6 @@ function paint(e) {
   }
 }
 
-
 function toggleColor(e) {
   let array = Array.from(drawingBoard.children)
   array.forEach((e) => { e.removeEventListener('mouseenter', paint); })
@@ -80,16 +86,62 @@ function toggleColor(e) {
 function clearBoard() {
   let array = Array.from(drawingBoard.children)
   array.forEach((e) => {
-     e.style.backgroundColor = bgColor;
-     e.classList.remove('painted'); 
+    e.style.backgroundColor = bgColor;
+    e.classList.remove('painted');
   })
 
 }
 
-function selectEraser(e) {
-  if (color != bgColor) {
-    color = bgColor;
-  } else {
-    selectColor();
+
+
+function createRainbow(){
+  const r = Math.floor((Math.random() * 255) + 1);
+  const g = Math.floor((Math.random() * 255) + 1);
+  const b = Math.floor((Math.random() * 255) + 1);
+  return `rgba(${r}, ${g}, ${b})`;
+}
+
+
+function hexToRGB(hex) {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+  return `rgba(${r}, ${g}, ${b}, ${0.6})`;
+}
+
+function selectMode(selected) {
+  switch (selected) {
+    case 'colorButton':
+      eraser.classList.remove('clickedEraser');
+      rainbow.classList.remove('clickedRainbow');
+      brushColorButton.classList.add('clickedBrushColorButton');
+      break;
+    case 'eraserButton':
+      brushColorButton.classList.remove('clickedBrushColorButton');
+      rainbow.classList.remove('clickedRainbow');
+      eraser.classList.add('clickedEraser');
+      break;
+    case 'rainbowButton':
+      rainbow.classList.add('clickedRainbow');
+      brushColorButton.classList.remove('clickedBrushColorButton');
+      eraser.classList.remove('clickedEraser');
+      break;
+  }
+}
+
+function colorSelector(){
+  switch(true) {
+    case (rainbow.classList == 'clickedRainbow'):
+      color = createRainbow();
+      return color;
+      break;
+    case (eraser.classList == 'clickedEraser'):
+      color = bgColor;
+      return color;
+      break;
+    case (brushColorButton.classList == 'clickedBrushColorButton'):
+      color = brushColor.value;
+      return color;
+      break;
   }
 }
